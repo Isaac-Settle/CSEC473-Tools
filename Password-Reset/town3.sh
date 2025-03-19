43bb493d7de78fb7ddb4459021228105dc0c34e0
Isaac S. <89415690+Isaac-Settle@users.noreply.github.com>
2025-03-19 14:03:46 -0400

    Update town3.sh

--- Password-Reset/town3.sh
+++ Password-Reset/town3.sh
@@ -10,5 +10,4 @@ ssh-keyscan -H 10.10.3.1 >> ~/.ssh/known_hosts
 #sshpass -p "test" ssh LCoramar@192.168.178.102 'powershell "New-LocalUser -Name "test3" -Password (ConvertTo-SecureString "test" -AsPlainText -Force)"; "echo test3 > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
 
 # Uses sshpass to ssh to an IP using the default provided password and executes a powershell command to change the password.
-sshpass -p "H3ralds_Tom3" ssh LSeelie@10.10.3.1 'powershell "Set-LocalUser -Name "LSeelie" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
-sshpass -p "shadow" ssh monk@10.10.3.1 'powershell "Set-LocalUser -Name "monk" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
+sshpass -p "H3ralds_Tom3" ssh LSeelie@10.10.3.1 'powershell "Set-LocalUser -Name "LSeelie" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "Set-LocalUser -Name "monk" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
