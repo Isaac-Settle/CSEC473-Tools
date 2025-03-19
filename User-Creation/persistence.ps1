1d7578b537e619779ae0812775c515e55696d987
Isaac-Settle <89415690+Isaac-Settle@users.noreply.github.com>
2025-03-18 22:24:40 -0400

    Update persistence.ps1

--- User-Creation/persistence.ps1
+++ User-Creation/persistence.ps1
@@ -1,13 +1,13 @@
 ﻿$credentials= @{
     username = "foxtrot1"
-    password = ConvertTo-SecureString "Password" -AsPlainText -Force
+    password = ConvertTo-SecureString "f0xtr0t_backup_PA55" -AsPlainText -Force
     username2 = "foxtrot2"
-    password2 = ConvertTo-SecureString "Password" -AsPlainText -Force
+    password2 = ConvertTo-SecureString "f0xtr0t2_backup_PA55" -AsPlainText -Force
     username3 = "foxtrot3"
-    password3 = ConvertTo-SecureString "Password" -AsPlainText -Force
+    password3 = ConvertTo-SecureString "f0xtr0t3_backup_PA55" -AsPlainText -Force
     username4 = "foxtrot4"
-    password4 = ConvertTo-SecureString "Password" -AsPlainText -Force
+    password4 = ConvertTo-SecureString "f0xtr0t4_backup_PA55" -AsPlainText -Force
     username5 = "foxtrot5"
-    password5 = ConvertTo-SecureString "Password" -AsPlainText -Force
+    password5 = ConvertTo-SecureString "f0xtr0t5_backup_PA55" -AsPlainText -Force
 }
 $credentials | Export-Clixml -Path C:\Windows\System32\fi-FI\credentials.xml
