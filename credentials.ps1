7b650de6a51f34c53c6518461949947b5ff3154d
Isaac-Settle <89415690+Isaac-Settle@users.noreply.github.com>
2025-03-18 20:18:59 -0400

    Create credentials.ps1

---
+++ credentials.ps1
@@ -0,0 +1,4 @@
+cmdkey /add:10.10.1.1 /user:CAgrupnin /pass:f0xtr0t_w1nd0ws_PA55
+cmdkey /add:10.10.2.1 /user:LCoramar /pass:f0xtr0t_w1nd0ws_PA55
+cmdkey /add:10.10.3.1 /user:LSeelie /pass:f0xtr0t_w1nd0ws_PA55
+cmdkey /add:10.10.4.1 /user:VChloras /pass:f0xtr0t_w1nd0ws_PA55
