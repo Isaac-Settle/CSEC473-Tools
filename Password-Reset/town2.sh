836a010abe86d54a96476cf545906023947def4c
Isaac S. <89415690+Isaac-Settle@users.noreply.github.com>
2025-03-19 14:03:18 -0400

    Update town2.sh

--- Password-Reset/town2.sh
+++ Password-Reset/town2.sh
@@ -10,5 +10,4 @@ ssh-keyscan -H 10.10.2.1 >> ~/.ssh/known_hosts
 #sshpass -p "test1" ssh LCoramar@192.168.178.102 'powershell "New-LocalUser -Name "test2" -Password (ConvertTo-SecureString "test" -AsPlainText -Force)"; "echo test2 > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
 
 #Uses sshpass to ssh to an IP using the default provided password and executes a powershell command to change the password.
-sshpass -p "@rchitect_@rcane" ssh LCoramar@10.10.2.1 'powershell "Set-LocalUser -Name "LCoramar" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
-sshpass -p "ghostslayer" ssh bloodhunter@10.10.2.1 'powershell "Set-LocalUser -Name "bloodhunter" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > \system32\Grey-team\logbook.txt"; shutdown /r /t 0'
+sshpass -p "@rchitect_@rcane" ssh LCoramar@10.10.2.1 'powershell "Set-LocalUser -Name "LCoramar" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "Set-LocalUser -Name "bloodhunter" -Password (ConvertTo-SecureString "f0xtr0t_w1nd0ws_PA55" -AsPlainText -Force)"; "echo Vanguard > /system32/Grey-team/logbook.txt"; shutdown /r /t 0'
